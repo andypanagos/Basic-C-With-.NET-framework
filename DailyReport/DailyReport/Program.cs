6cc2c5988f3d8ffbf9e28bc1a2b8a934ee4be7d5
andypanagos <andypanagos2@gmail.com>
2022-11-17 17:15:43 -0500

    DailyReport

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy \n \nStudent Daily Report");
+            Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
+        }
+    }
+}
