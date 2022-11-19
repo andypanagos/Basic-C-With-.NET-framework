e764896e6e4df196a0e65e9e4d6b801d2fc14717
andypanagos <andypanagos2@gmail.com>
2022-11-19 15:55:38 -0500

    Boolean Assignment

---
+++ BooleanAssignment/BooleanAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age, ticket;
+
+            Console.WriteLine("What is your age?");
+            age = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Have you ever had a DUI?");
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets have you had?");
+            ticket = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            Console.ReadLine();
+
+            bool dui = false;
+            bool T = ticket <= 1;
+            bool qualified = (age >= 15 || dui || T);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //all have to be true
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            //one needs to be true, not both
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+        }
+    }
+}
