f911856b28b478aa40601913fc20eb2cc8df85dd
andypanagos <andypanagos2@gmail.com>
2022-11-27 17:03:05 -0500

    Main Method

---
+++ Main Method/Main Method/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Math
+    {
+        public int addition(int num1)
+        {
+            int results = num1 + 50;
+
+            return results;
+        }
+        public decimal addition(decimal num1)
+        {
+            decimal results = num1 / 5;
+
+            return results;
+        }
+        public int addition(string num1)
+        {
+            int results = Convert.ToInt32(num1) + 100;
+
+
+            return results;
+        }
+    }
+}
---
+++ Main Method/Main Method/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mathOperations = new Math();
+            int num1 = 50;
+
+            int answer1 = mathOperations.Math1(num1);
+
+            decimal answer2 = mathOperations.Math2(15m);
+
+            int answer3 = mathOperations.Math3("15");
+            Console.WriteLine("50 plus 50 equals: " + answer1);
+            Console.ReadLine();
+
+            Console.WriteLine("50 divided by 5 equals: " + answer2);
+            Console.ReadLine();
+
+            Console.WriteLine("50 plus 100 equals: " + answer3);
+            Console.ReadLine();
+
+
+        }
+    }
+}
