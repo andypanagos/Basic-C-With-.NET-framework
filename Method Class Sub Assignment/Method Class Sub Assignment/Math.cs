5118af9df7bbdaed0d9798bf79a1f07902f2b6bd
andypanagos <andypanagos2@gmail.com>
2022-11-27 18:26:01 -0500

    Method Class

---
+++ Method Class Sub Assignment/Method Class Sub Assignment/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Sub_Assignment
+{
+    public class Math
+    {
+        
+        public void Mathop(int num1, int num2)
+        {
+            int result = num1 + 10;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ Method Class Sub Assignment/Method Class Sub Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Sub_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number: ");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second number: ");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+
+            Math newMath = new Math();
+
+            newMath.Mathop(4, 9);
+
+            newMath.Mathop(numOne, numTwo);
+            Console.ReadLine();
+        }
+       
+    }
+    
+}
