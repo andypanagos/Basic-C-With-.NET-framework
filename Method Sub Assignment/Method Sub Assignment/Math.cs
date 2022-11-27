00404f6abe05057a27891c095d77688e6c45c60f
andypanagos <andypanagos2@gmail.com>
2022-11-27 18:06:54 -0500

    Method Sub Assignment

---
+++ Method Sub Assignment/Method Sub Assignment/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Sub_Assignment
+{
+    
+    
+        public class Math
+        {
+            public int MathMethod(int x, int y = 0)
+            {
+                int result = (x * y) / 2;
+                return result;
+            }
+        }
+
+ 
+        
+    
+}
---
+++ Method Sub Assignment/Method Sub Assignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Sub_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate
+            Math newMath = new Math();
+
+            //Ask for numbers
+            Console.WriteLine("Enter a number... ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter another number. Optional.");
+            string opAnswer = null;
+            int tempNum = 0;
+            //try loop using the integer provided, turning it into a temporary variable
+            try
+            {
+                opAnswer = Console.ReadLine();
+                tempNum = Convert.ToInt32(opAnswer);
+            }
+            catch(Exception)
+            {
+            }
+            int num2 = tempNum;
+            int total;
+
+            if(opAnswer == null)
+            {
+                total = newMath.MathMethod(num1);
+                Console.WriteLine("Your total is: {0}", total);
+            }
+            else
+            {
+                total = newMath.MathMethod(num1, num2);
+                Console.WriteLine("total is: {0}. and second number is: {1}", total, num2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
