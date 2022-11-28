18e29feacaa9ae05eeab5233fe98002ab12e8a4e
andypanagos <andypanagos2@gmail.com>
2022-11-28 14:06:10 -0500

    Class void pt 2 Assignment

---
+++ Class Sub Assignment/Class Sub Assignment/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Sub_Assignment
+{
+    class Class1
+    {
+        //void method that divides by 2
+        public void Math1(int num = 0)
+        {
+            int num1 = (num / 2);
+            Console.WriteLine(num1);
+        }
+
+        //method with output parameters
+        public void Math2(out int numOut)
+        {
+            numOut = 50;
+        }
+
+        //Overload double method
+        public void Math3(double num2)
+        {
+            double numdub = num2 / 4;
+            Console.WriteLine(numdub);
+        }
+    }
+}
---
+++ Class Sub Assignment/Class Sub Assignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Sub_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 firstclass = new Class1();
+
+            Console.WriteLine("Enter a number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your number divided by 2 is: ");
+            firstclass.Math1(x);
+            Console.ReadLine();
+
+            int num = 0;
+
+            firstclass.Math2(out num);
+            Console.WriteLine(num);
+
+            Class1 secondClass = new Class1();
+            Console.WriteLine("Enter decimal number: ");
+            
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("your decimal number divided by 4 is: ");
+            secondClass.Math3(y);
+
+
+            Console.ReadLine();
+            
+        }
+    }
+}
