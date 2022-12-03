214d91981fbb3b9c606d89b7643dfbd405566375
andypanagos <andypanagos2@gmail.com>
2022-12-03 16:07:44 -0500

    Abstract Class

---
+++ Abstract Class/Abstract Class/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    public class Employee : Person
+    {
+        //inherits sayname method from abstract person class
+        public virtual void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ Abstract Class/Abstract Class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating employee class with first and last name objects inherited from person class.
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.Read();
+        }
+    }
+}
