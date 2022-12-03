91e21c8c45028d32ddf93bf9ff897f8b2dd697ac
andypanagos <andypanagos2@gmail.com>
2022-12-03 00:18:25 -0500

    fixed class methods

--- Main Method/Main Method/Math.cs
+++ Main Method/Main Method/Math.cs
@@ -8,19 +8,19 @@ namespace Main_Method
 {
     class Math
     {
-        public int addition(int num1)
+        public int Math1(int num1)
         {
             int results = num1 + 50;
 
             return results;
         }
-        public decimal addition(decimal num1)
+        public decimal Math2(decimal num1)
         {
             decimal results = num1 / 5;
 
             return results;
         }
-        public int addition(string num1)
+        public int Math3(string num1)
         {
             int results = Convert.ToInt32(num1) + 100;
 
