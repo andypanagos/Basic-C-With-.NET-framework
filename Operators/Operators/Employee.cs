8cb6e45606babb47390f71eb99394f9672f90ecd
andypanagos <andypanagos2@gmail.com>
2022-12-03 17:41:23 -0500

    Operators

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public static bool operator ==(Employee empOne, Employee empTwo)
+        {
+            if (empOne.ID == empTwo.ID) 
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Employee empOne, Employee empTwo)
+        {
+            return !(empOne.ID == empTwo.ID);
+        }
+        
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee empOne = new Employee();
+            empOne.FirstName = "Andy";
+            empOne.LastName = "Panagos";
+            empOne.ID = 11;
+
+            Employee empTwo = new Employee();
+            empTwo.FirstName = "Griffin";
+            empTwo.LastName = "Ancevic";
+            empTwo.ID = 13;
+
+            Console.WriteLine("Is griffin's ID the same as Andy's");
+            Console.WriteLine(empTwo.ID == empOne.ID);
+            Console.ReadLine();
+
+        }
+    }
+}
