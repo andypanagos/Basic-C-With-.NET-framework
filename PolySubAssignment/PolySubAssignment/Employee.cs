fb4a42972766015e6147b983a4cb83c6ea1f023a
andypanagos <andypanagos2@gmail.com>
2022-12-03 16:25:42 -0500

    Polymorphism Sub Assignment

---
+++ PolySubAssignment/PolySubAssignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolySubAssignment
+{
+    public class Employee : Iquitt
+    {
+     
+        public void Quit()
+        {
+            Console.WriteLine("You quit.");
+        }
+    }
+}
---
+++ PolySubAssignment/PolySubAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolySubAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Iquitt quit = new Employee();
+            quit.Quit();
+            Console.ReadLine();
+        }
+    }
+}
