92be452f3b3b652656abbc60b8a266c89d5aad29
andypanagos <andypanagos2@gmail.com>
2022-12-03 15:11:35 -0500

    Methods and objects

---
+++ Methods And Objects/Methods And Objects/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_And_Objects
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("name is " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ Methods And Objects/Methods And Objects/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_And_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee newEmp = new Employee();
+            newEmp.FirstName = "Sample";
+            newEmp.LastName = "Student";
+            newEmp.SayName();
+            Console.ReadLine();
+        }
+        
+    }
+}
