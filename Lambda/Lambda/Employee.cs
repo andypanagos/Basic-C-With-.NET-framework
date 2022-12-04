057c6d1b2284c6257c565f16754dc95a1d8e11f2
andypanagos <andypanagos2@gmail.com>
2022-12-04 14:51:55 -0500

    Lambda Sub Assinment

---
+++ Lambda/Lambda/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    public class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public int ID;
+
+    }
+}
---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> EmployeeList = new List <Employee>();
+            {
+                EmployeeList.Add(new Employee() { FirstName = "Joe", LastName = "Coffee", ID = 1 });
+                EmployeeList.Add(new Employee() { FirstName = "Mac", LastName = "Jones", ID = 2 });
+                EmployeeList.Add(new Employee() { FirstName = "Lebron", LastName = "James", ID = 3 });
+                EmployeeList.Add(new Employee() { FirstName = "Floyd", LastName = "Mayweather", ID = 4 });
+                EmployeeList.Add(new Employee() { FirstName = "Elon", LastName = "Musk", ID = 5 });
+                EmployeeList.Add(new Employee() { FirstName = "Joe", LastName = "Rogan", ID = 6 });
+                EmployeeList.Add(new Employee() { FirstName = "Alex", LastName = "Smith", ID = 7 });
+                EmployeeList.Add(new Employee() { FirstName = "Pat", LastName = "Johnson", ID = 8 });
+                EmployeeList.Add(new Employee() { FirstName = "Mike", LastName = "Williams", ID = 9 });
+                EmployeeList.Add(new Employee() { FirstName = "Ben", LastName = "Simmonds", ID = 10 });
+
+                Console.WriteLine("Employees named Joe: ");
+                Console.ReadLine();
+
+                Console.WriteLine("\n");
+
+                foreach(Employee employee in EmployeeList)
+                {
+                    if ( employee.FirstName == "Joe")
+                    {
+                        Console.WriteLine(employee.FirstName + " " + employee.LastName);
+                    }
+                }
+                Console.ReadLine();
+
+                
+
+                Console.WriteLine("Employees with ID greater than 5");
+                Console.ReadLine();
+
+                List<Employee> EmployeeList3 = EmployeeList.Where(x => x.ID > 5).ToList();
+                foreach (Employee employee in EmployeeList3)
+                {
+                    Console.WriteLine(employee.FirstName + " " + employee.LastName + " " + employee.ID);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
