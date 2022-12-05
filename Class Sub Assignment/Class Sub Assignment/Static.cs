4063483f65ebe0e46885ce8ab270bfb8ce695689
andypanagos <andypanagos2@gmail.com>
2022-12-05 13:34:34 -0500

    Created Static class

---
+++ Class Sub Assignment/Class Sub Assignment/Static.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Sub_Assignment
+{
+    static class Static
+    {
+        //declared static class to hold static methods
+    }
+}
