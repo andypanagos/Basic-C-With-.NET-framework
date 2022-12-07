cfd2b211948aef9dc9f302a3bfff796b6ed90874
andypanagos <andypanagos2@gmail.com>
2022-12-06 23:20:03 -0500

    Constructor Sub Assignment

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string conString = "Hello";
+            Console.WriteLine(conString);
+
+            var mySelf = "Andy";
+
+            user sayUser = new user();
+            sayUser.SayName();
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Constructor/Constructor/user.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class user
+    {
+        string FirstName;
+        string LastName;
+
+        public user() : this("jayson", "tatum") { }
+        //chaining two constructs
+        public user(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+
+        }
+
+        public void SayName()
+        {
+            Console.WriteLine("MVP: " + FirstName + " " + LastName);
+        }
+    }
+}
